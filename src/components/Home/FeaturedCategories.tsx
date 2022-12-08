daeca0e43b008ff798c942f23b07cb020087d4d5
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-08 09:07:20 +0600

    refactor(components/Home): reuse hooks

--- src/components/Home/FeaturedCategories.tsx
+++ src/components/Home/FeaturedCategories.tsx
@@ -1,72 +1,34 @@
-import { api } from "../../util/api";
-import { useEffect, useState } from "react";
-import { formatError } from "../../util/format-error";
-
 import { Category } from "./Category";
-import { Alert } from "../other/Alert";
-import { LoadingSpinner } from "../other/LoadingSpinner";
-
+import { useFetchData } from "../../util/hooks";
 import type { CategoryPublicInterface } from "../../interfaces/category";
+import { processFetchedDataState } from "../../util/process-fetched-data";
 
-type CategoryState =
-  | { status: "loading"; data: null }
-  | { status: "error"; message: string }
-  | { status: "loaded"; data: CategoryPublicInterface[] };
+const categoryWrapperStyle = Object.freeze({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+} as const);
 
 export function FeaturedCategories() {
-  const [categoryState, setCategoryState] = useState<CategoryState>({
-    status: "loading",
-    data: null,
+  const categoryState = useFetchData<CategoryPublicInterface[]>({
+    path: "categories",
+    query: { lookup: "all" },
+    processResponseData: (categories) =>
+      categories.filter((category) => category.imageUrl),
   });
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await api.get<CategoryPublicInterface[]>({
-          path: "categories",
-          query: { lookup: "all" },
-        });
-
-        if (!response.success)
-          return setCategoryState({
-            status: "error",
-            message: formatError({ errorResponse: response! }),
-          });
-
-        setCategoryState({
-          status: "loaded",
-          data: response.data.filter((category) => category.imageUrl),
-        });
-      } catch (ex) {
-        setCategoryState({ status: "error", message: ex.message });
-      }
-    })();
-  }, []);
-
-  const categoryWrapperStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-  } as const;
-
-  const categoryResult = (() => {
-    switch (categoryState.status) {
-      case "loading":
-        return <LoadingSpinner />;
-
-      case "error":
-        return <Alert message={categoryState.message} type={"danger"} />;
-
-      default:
-        return (
-          <div style={categoryWrapperStyle}>
-            {categoryState.data.map((category) => (
-              <Category key={category._id} category={category} />
-            ))}
-          </div>
-        );
-    }
-  })();
+  const categoryResult = processFetchedDataState<CategoryPublicInterface[]>({
+    state: categoryState,
+    processLoadedData(categories) {
+      return (
+        <div style={categoryWrapperStyle}>
+          {categories.map((category) => (
+            <Category key={category._id} category={category} />
+          ))}
+        </div>
+      );
+    },
+  });
 
   return (
     <div className="container text-center">
