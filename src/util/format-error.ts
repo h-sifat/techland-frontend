bd197bb1bcd5feb1fc63c9abd4411073a00f6cac
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-08 07:08:15 +0600

    feat(util): add api error response formatter

---
+++ src/util/format-error.ts
@@ -0,0 +1,21 @@
+import type { API_Error_Response } from "../interfaces/api";
+
+export interface FormatError_Argument {
+  errorResponse: API_Error_Response;
+}
+export function formatError(arg: FormatError_Argument) {
+  const { errorResponse } = arg;
+
+  if (errorResponse.errorType === "msgAndCode")
+    return errorResponse.error.message;
+
+  {
+    const { formErrors } = errorResponse.error;
+    if (formErrors.length) return formErrors[0];
+  }
+
+  {
+    const { fieldErrors } = errorResponse.error;
+    for (const field in fieldErrors) return fieldErrors[field][0];
+  }
+}
