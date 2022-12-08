5eb34a1c6c6f19a793776d3cc6ad512c8dc1a96b
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-08 09:57:57 +0600

    feat: add root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Footer } from "./components/Footer";
+import { Route } from "wouter";
+import { config } from "./config";
+
 import { NavBar } from "./components/Nav";
+import { Footer } from "./components/Footer";
+import { Home } from "./components/Home/index";
+import { Products } from "./components/Products";
 
 export function App() {
   return (
     <>
       <NavBar />
 
-      <div
-        className="container my-4 center"
-        style={{ minHeight: "90vh" }}
-      ></div>
+      <div className="container my-4 center" style={{ minHeight: "90vh" }}>
+        <Route path="/" component={Home} />
+        <Route path={config.PRODUCTS_PAGE_PATH} component={Products} />
+      </div>
 
       <Footer />
     </>
