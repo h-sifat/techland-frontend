c705780508ab3144d9f9894c7c12963d8c4884f4
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-08 23:38:11 +0600

    fix(components/Products): product links

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,6 +1,8 @@
 import { BsCart } from "react-icons/bs";
 import { priceUnitMap } from "../../util/product";
 import { FindResult } from "../../interfaces/product";
+import { getPageUrl } from "../../util/url";
+import { Link } from "wouter";
 
 export interface ProductCard_Argument {
   product: FindResult["products"][number];
@@ -9,27 +11,31 @@ export function ProductCard(arg: ProductCard_Argument) {
   const { product } = arg;
 
   const { name, price, priceUnit, shortDescriptions, imageUrl } = product;
+  const productUrl = getPageUrl({
+    page: "/product",
+    query: { qType: "byIds", ids: [product._id] },
+  });
 
   return (
     <div
       className="card shadow h-100 border border-2"
       style={{ width: "15rem", fontSize: "0.86rem" }}
     >
-      <a href="#">
+      <Link href={productUrl}>
         <img
           src={imageUrl}
           crossOrigin="anonymous"
           className="card-img-top"
           style={{ height: "10rem", objectFit: "cover" }}
         />
-      </a>
+      </Link>
       <div className="card-body pb-0">
-        <a
-          href="#"
+        <Link
+          href={productUrl}
           className="text-decoration-none card-title fw-semibold d-block mb-0"
         >
           {name}
-        </a>
+        </Link>
         <ul className="p-3 pb-0 mb-1">
           {shortDescriptions.slice(0, 4).map((text, index) => (
             <li key={index} className=" p-0">
