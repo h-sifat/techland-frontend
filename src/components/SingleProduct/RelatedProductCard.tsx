ef47d2bf5e0b0dd232f78e87e496400bd57962b6
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-08 23:07:19 +0600

    feat(components/SingleProduct): +related prod card

---
+++ src/components/SingleProduct/RelatedProductCard.tsx
@@ -0,0 +1,56 @@
+import { config } from "../../config";
+import { getPageUrl } from "../../util/url";
+import { priceUnitMap } from "../../util/product";
+import { RelatedProductDocument } from "../../interfaces/product";
+
+export interface RelatedProductCard_Argument {
+  product: RelatedProductDocument;
+}
+export function RelatedProductCard(arg: RelatedProductCard_Argument) {
+  const { product } = arg;
+
+  const badgeInfo = {
+    Price: `${product.price} ${priceUnitMap[product.priceUnit]}`,
+    Brand: product.brand.name || "Non-Brand",
+  };
+
+  const productUrl = getPageUrl({
+    page: config.SINGLE_PRODUCT_PAGE,
+    query: { qType: "byIds", ids: [product._id] },
+  });
+
+  return (
+    <div className="col col-12 card mb-3 mx-auto px-0 shadow-sm">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={product.imageUrl}
+            crossOrigin="anonymous"
+            style={{ objectFit: "cover" }}
+            className="img-fluid rounded-start h-100 w-100"
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">
+              <a href={productUrl} className="text-decoration-none">
+                {product.name}
+              </a>
+            </h5>
+            <p className="card-text">
+              {Object.entries(badgeInfo).map(([name, value]) => (
+                <span
+                  key={name}
+                  className="badge rounded-pill text-bg-light fw-normal mx-1"
+                >
+                  {name}: <span className="fw-bold">{value}</span>
+                </span>
+              ))}
+            </p>
+            <p className="card-text"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 export interface Config {
   API_ROOT: string;
   PRODUCTS_PAGE_PATH: string;
+  SINGLE_PRODUCT_PAGE: string;
   MAX_SEARCH_QUERY_LENGTH: number;
   DEFAULT_PRODUCTS_PER_PAGE: number;
 }
@@ -9,5 +10,6 @@ export const config: Config = Object.freeze({
   MAX_SEARCH_QUERY_LENGTH: 150,
   DEFAULT_PRODUCTS_PER_PAGE: 20,
   PRODUCTS_PAGE_PATH: "/products",
+  SINGLE_PRODUCT_PAGE: "/product",
   API_ROOT: "http://127.0.0.1:3000/api-v0-1-0/",
 });
